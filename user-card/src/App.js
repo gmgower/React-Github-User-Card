f69b2be9b3f571045b413b699012619f449f2257
Mada Gower <gmgower@gmail.com>
2019-09-07 16:04:20 -0500

    Refactor code to render followers in UserList.

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,47 +1,47 @@
 import React from 'react';
 import './App.css';
-import UserCard from './components/UserCard.jsx'
-import axios from 'axios'
+import UserCard from './components/UserCard.jsx';
+
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       user: {},
-      followers: [],
+      followers: []
     };
   }
 
-  componentDidMount() {    
-    this.usersGet()
-    this.usersFollowers()
+  componentDidMount() {
+    this.usersGet();
+    this.usersFollowers();
   }
 
   usersGet = () => {
-    axios.get('https://api.github.com/users/gmgower')
-    // .then(res => res.json())
-    .then(res => {
-      console.log(res)
-      this.setState({user: res.data})
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }
+    axios
+      .get('https://api.github.com/users/gmgower')
+      // .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({ user: res.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   usersFollowers = () => {
-    axios.get('https://api.github.com/users/gmgower/followers')
-    .then(res => {
-      console.log(res.data)
-      this.setState({followers: res.data})
-    })
-  }
-
+    axios.get('https://api.github.com/users/gmgower/followers').then(res => {
+      console.log(res.data);
+      this.setState({ followers: res.data });
+    });
+  };
 
   render() {
     return (
       <div className='App'>
-        <UserCard user={this.state.user} followers={this.state.followers}/>       
+        <UserCard user={this.state.user} followers={this.state.followers} />
       </div>
     );
   }
--- user-card/src/components/UserCard.jsx
+++ user-card/src/components/UserCard.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import {Card, Icon, Image} from 'semantic-ui-react'
+import React from 'react';
+import UserList from './UserList';
 
-const UserCard = (props) => {
-    console.log('UserCard Props', props)
-    return (
-        <div>
-        <Card>
-            <Card.Content>
-                <Image src={props.user.avatar_url}/>
-            <Card.Header>User Login: {props.user.login}</Card.Header> 
-            <Card.Meta>User Location: {props.user.location}</Card.Meta>
-            <p>User Following: {props.user.following}</p>        
-            <p>User Followers: {props.user.followers}</p>
-            </Card.Content>   
-            </Card>        
-            <div>
-                {props.followers.map( follower => (
-                    <div key={follower.id}>{follower.login}</div>
-                ))}
-            </div>
-            </div>
-    )
-}
+import { Card, Icon, Image } from 'semantic-ui-react';
 
-export default UserCard
+const UserCard = props => {
+  console.log('UserCard Props', props);
+  return (
+    <div>
+      <Card>
+        <Card.Content>
+          <Image src={props.user.avatar_url} />
+          <Card.Header>User Login: {props.user.login}</Card.Header>
+          <Card.Meta>User Location: {props.user.location}</Card.Meta>
+          <p>User Following: {props.user.following}</p>
+          <p>User Followers: {props.user.followers}</p>
+        </Card.Content>
+
+        <UserList user={props.user} followers={props.followers} />
+      </Card>
+    </div>
+  );
+};
+
+export default UserCard;

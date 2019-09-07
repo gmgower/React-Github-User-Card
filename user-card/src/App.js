42456f89a1ba073fc2ffa1c16dea8828f377ef5c
Mada Gower <gmgower@gmail.com>
2019-09-06 19:06:11 -0500

    Setup class compoenent & instal axios.

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
